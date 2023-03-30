27a4e5eb081457af866691ae3db3d78744db1cde
Matt <mhgarry92@gmail.com>
2023-03-30 19:56:15 -0400

    restart

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,23 @@
 //As a coding student I would like to make a quiz to show off my knowledge on the fundementals of coding
+//passing in array of questions from seperate file 
 
 //variables we need throughtout the entire quiz 
-let time = document.querySelector ("#count");
+let time = document.querySelector("#count");
 const quizDisplay = document.getElementById("display-box");
-const startBtn = document.querySelector ("#start-button");
-const submitBtn = document.querySelector ("#submit");
+const startBtn = document.querySelector("#start-button");
+const submitBtn = document.querySelector("#submit");
+let score = 0;
+let timer;
+let currentQuestion = 0;
+const questions = questionsArr;
 //created array in seperate doc with help from tutoring session with Janvier Mbilizi
 
 //a variable to keep track of score 
-let score = 0;
 
-
-let timer;
 
 
 //starts our quiz by hiding the start button and intro paragraph as well as starting the timer 
-async function startQuiz() {
+ function startQuiz() {
   let timer = 100;
   //hide welcome paragraph 
   let paragraph = document.getElementById("welcome-text");
@@ -44,19 +46,15 @@ const countdown = setInterval(async function () {
 //made a funciton to put together the quiz using the questionsArr array data
 function buildQuiz() {
   // get the current question and its answers from the array
-      //a variable to keep track of current question indexed 
-      
-    const questions = questionsArr;
-    const currentQuestion = [0]
-
-  
+      //a variable to keep track of current question indexed and to define array from other page
+  const questions = questionsArr;
+  const currentQuestion = 0
+   //creating variables to hold the current question and current answers
   const currentQ = questions[currentQuestion];
   const qText = currentQ.question;
   const aChoices = currentQ.answers;
-  
   // display the current question
   document.getElementById("question-title").textContent = qText;
-  
   // create radio buttons for each answer choice I can't currently get this to work 
   const choiceDisplay = document.getElementById("choices");
   choiceDisplay.innerHTML = '';
@@ -65,9 +63,9 @@ function buildQuiz() {
     const radio = document.createElement('input');
     radio.type = 'radio';
     radio.name = 'answers' + currentQuestion;
-    radio.value = aChoices[currentQuestion];
+    radio.value = aChoices[i].text;
     label.appendChild(radio);
-    label.appendChild(document.createTextNode(aChoices[currentQuestion]));
+    label.appendChild(document.createTextNode(aChoices[i]));
     choiceDisplay.appendChild(label);
   }
 }
@@ -75,17 +73,17 @@ function buildQuiz() {
 // function to check the user's answer
 function checkAnswer() {
   // get the selected answer
-  const answer = document.querySelector('input[name="question' + currentQuestion + '"]:checked');
-  
+  const answer = document.querySelector('input[name="answers' + currentQuestion - 1 + '"]:checked');
   if (answer) {
-    // check if the answer is correct
-    if (answer.value === questions[currentQuestion].correctAnswer) {
+    const questions = questionsArr; // get the current question and its answers from the array
+    // check if the answer is correct and add to the score if it is correct
+    if (answer.value === questions[currentQuestion -1].correctAnswer) {
       score++;
     }
     
     // move to the next question or end the quiz if all questions have been answered
     currentQuestion++;
-    if (currentQuestion === questions.length) {
+    if (currentQuestion > questions.length) {
       endQuiz();
     } else {
       buildQuiz();
