9c4706de027580e70088ba9acb8b648422ed6da1
Matt <mhgarry92@gmail.com>
2023-04-07 21:32:29 -0400

    hide/start

--- Develop/script.js
+++ Develop/script.js
@@ -1,101 +1,158 @@
-//As a coding student I would like to make a quiz to show off my knowledge on the fundementals of coding
-//passing in array of questions from seperate file 
+//moved questions array to main file and restarted javascript
+const questionsArr = [
+  {
+    question:
+    "What is HTML's main use in Wev Development?",
+    answers: [
+    {Option: "Scripting", correct: false},
+    {Option: "Styling", correct: false},
+    {Option: "Debugging", correct: false},
+    {Option: "Structuring", correct: true},
+    ],
+  },
+  {
+  question:
+  "What is it called when Web Developers organize code in a readable and functional manner?",
+  answers:
+  [
+    { Option: "JavaScript", correct: false },
+    {Option: "API", correct: false},
+    {Option: "Refactoring", correct: true},
+    {Option: "Interfacing", correct: false},
+  ]
+  },
+    //https://www.w3schools.com/java/java_data_types_non-prim.asp#:~:text=A%20primitive%20type%20has%20always,have%20all%20the%20same%20size.
+  {
+    question:
+     "In JavaScript which of the following is not a primative values?",
+    answers:
+    [
+      {Option: "Number", correct: false},
+      {Option: "Function", correct: true},
+      {Option: "Boolean",  correct: false},
+      {Option: "String", correct: false},
+    ],
+    },
+  {
+    question:
+    "In CSS which of the following are used as selectors?",
+    answers:
+    [
+    {Option: "ID", correct: true},
+    {Option: "Class", correct: false},
+    {Option: "Global", correct: false},
+    {Option: "They're all selectors", correct: false},
+      ],
 
-//variables we need throughtout the entire quiz 
-let time = document.querySelector("#count");
-const quizDisplay = document.getElementById("display-box");
-const startBtn = document.querySelector("#start-button");
-const submitBtn = document.querySelector("#submit");
-let score = 0;
-let timer;
-let currentQuestion = 0;
-const questions = questionsArr;
-//created array in seperate doc with help from tutoring session with Janvier Mbilizi
+  },
+  {
+    question:
+    'What is the planning and drawing out the design of a website before being built called?',
+    answers:
+    [
+    {Option: "Keyframing", correct: false},
+    {Option: "Githubbing", correct: false},
+    {Option: "Storyinboarding", correct: false},
+    {Option: "Wireframing", correct: true},
+      ],
+  },
+  {
+     //paraphrased and forumlated from https://developer.mozilla.org/en-US/docs/Glossary/Element
+    question:
+     'What is a typical element in Web Design?',
+    answers:
+    [
+    {Option: "A key value pair", correct: false},
+    { Option: "Any part of a webpage that includes an opening tag with attrtirutes, encolsoed text, and a closing tag", correct: true},
+      {Option: "A Boolean", correct: false},
+      {Option: "A google", correct: false},
+       ],
+  },
+  {
+    //paraphrased from https://docs.github.com/en/repositories/creating-and-managing-repositories/about-repositories
+    question:
+    'A github repository is?',
+    answers:
+    [
+    {Option: "Cointains all fo your project's files and revision history", correct: true},
+    {Option: "Syntax", correct: false},
+   {Option: "A mystery", correct: false},
+   {Option: "A container for trash", correct: false},
+    ],
 
-//a variable to keep track of score 
 
+  },
+  {
+    //https://www.hackreactor.com/blog/what-is-javascript-used-for paraphrased and referenced
+    question:
+    'JavaScript is?',
+    answers:
+    [
+    {Option: "A cilent-side and server-side programming language that allows you to make web pages interactive", correct: true},
+   {Option: "A web application", correct: false},
+   {Option: "A node", correct: false},
+   {Option: "A cup of coffee", correct: false},
+   ],
 
+  },
+  {
+    //https://www.git-tower.com/learn/git/faq/git-bash#:~:text=Git%20Bash%2C%20quite%20simply%2C%20is,built%20for%20Unix%2Dstyle%2
+    question:
+    '"What is Git Bash?"',
+    answers:
+  [
+  {Option: "A Mac OSX application", correct: false},
+  {Option: "A server designated for party chat", correct: false},
+  {Option: "A Windows application designed for Git version control", correct: true},
+  {Option: "A website", correct: false},
+     ],
+  },
+  //https://www.stackpath.com/edge-academy/what-is-a-web-application/ referenced
+  {
+    question:
+    "What is a web application?",
+    answers:
+  [{Option: "A program downloaded from the internet that runs on your local system", correct: false},
+  {Option: "A browser based flash file", correct: false},
+  {Option: "An iphone accesory", correct: false},
+  {Option: "A computer program that runs in a web browsers and perfroms taask over the internet", correct: true},
+    ],
+  },
+]
 
-//starts our quiz by hiding the start button and intro paragraph as well as starting the timer 
- function startQuiz() {
-  let timer = 100;
-  //hide welcome paragraph 
-  let paragraph = document.getElementById("welcome-text");
-  paragraph.setAttribute("class", "hide");
-  //hide start button 
- startBtn.classList.add ("hide");
- //show quiz display 
- quizDisplay.style.display = "block";
-//start timer begins asynchornously so that wheen we deduct time off of the clock it is not deducted into our answer is callled 
-const countdown = setInterval(async function () {
-      if (timer > 0) {
-      timer--;
-      time.textContent = 'Time left: ' + timer + 'seconds';
-      } else {
-            clearinterval(countdown);
-            time.textContent = 'Time is up!' 
-            
-            }
-    }, 1000);
-      
-      buildQuiz();
+//our global variables
+let firstPage = document.getElementById ("first-page")
+let mainHeader = document.getElementById("main-header")
+let highScore = document.getElementById("stats")
+let timer = document.getElementById("time")
+let welcomeText = document.getElementById("welcome")
+let startBtn = document.getElementById("start-button")
+let container = document.getElementById("quiz-container")
+let questionsTitles = document.getElementById("question-titles")
+let answerChoices = document.getElementById("answer-choices")
+let nextBtn = document.getElementById("next")
+let submitBtn = document.getElementById("log-score")
+
+//array variables
+let currentQuestionIndex = 0;
+let time = questionsArr.length * 10;
+let timerId;
+
+//make function to hide elements I want hidden on dom and start timer and show questions
+function startQuiz() {
+	firstPage.setAttribute("class", "hidden")
+	mainHeader.setAttribute("class", "hidden")
+	welcomeText.setAttribute("class", "hidden")
+	startBtn.setAttribute("class", "hidden")
+	container.removeAttribute("class")
+	questionsTitles.removeAttribute("class")
+	answerChoices.removeAttribute("class")
+	nextBtn.removeAttribute("class")
+	submitBtn.removeAttribute("class")
+	timerId = setInterval(time, 1000)
+
+	timer.textContent = time
+
 }
 
-
-
-//made a funciton to put together the quiz using the questionsArr array data
-function buildQuiz() {
-  // get the current question and its answers from the array
-      //a variable to keep track of current question indexed and to define array from other page
-  const questions = questionsArr;
-  const currentQuestion = 0
-   //creating variables to hold the current question and current answers
-  const currentQ = questions[currentQuestion];
-  const qText = currentQ.question;
-  const aChoices = currentQ.answers;
-  // display the current question
-  document.getElementById("question-title").textContent = qText;
-  // create radio buttons for each answer choice I can't currently get this to work 
-  const choiceDisplay = document.getElementById("choices");
-  choiceDisplay.innerHTML = '';
-  for (let i = 0; i < aChoices.length; i++) {
-    const label = document.createElement('label');
-    const radio = document.createElement('input');
-    radio.type = 'radio';
-    radio.name = 'answers' + currentQuestion;
-    radio.value = aChoices[i].text;
-    label.appendChild(radio);
-    label.appendChild(document.createTextNode(aChoices[i]));
-    choiceDisplay.appendChild(label);
-  }
-}
-
-// function to check the user's answer
-function checkAnswer() {
-  // get the selected answer
-  const answer = document.querySelector('input[name="answers' + currentQuestion - 1 + '"]:checked');
-  if (answer) {
-    const questions = questionsArr; // get the current question and its answers from the array
-    // check if the answer is correct and add to the score if it is correct
-    if (answer.value === questions[currentQuestion -1].correctAnswer) {
-      score++;
-    }
-    
-    // move to the next question or end the quiz if all questions have been answered
-    currentQuestion++;
-    if (currentQuestion > questions.length) {
-      endQuiz();
-    } else {
-      buildQuiz();
-    }
-  }
-}
-
-// function to end the quiz
-function endQuiz() {
-  // hide the quiz section and display the user's score
-  quizContainer.style.display = 'none';
-  document.getElementById('result').textContent = 'You scored ' + score + ' out of ' + questions.length;
-}
-startBtn.addEventListener("click", startQuiz);
-submitBtn.addEventListener("click", checkAnswer);
+document.getElementById("start-button").addEventListener("click", startQuiz)
