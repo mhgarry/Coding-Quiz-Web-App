ab65e7f4ea8f58ec5f172b63f60a5c6d7e0493cb
Matt <mhgarry92@gmail.com>
2023-04-09 18:19:17 -0400

    gets to end screen/need to make function to log score

--- Develop/questions.js
+++ Develop/questions.js
@@ -1,64 +1,64 @@
 //moved array of questions, options, and answers to seperate sprict file to make it more scalable. Reformatted it and added a radio button to each option
 const questionsArr = [
     {
-      "question":"What is HTML's main use in Web Development?",
-			"choices": ["Scripting", "Styling", "Debugging", "Structuring"],
-			"answer": 3
+      question:"What is HTML's main use in Web Development?",
+			choices: ["Scripting", "Styling", "Debugging", "Structuring"],
+			answer: 3
     },
     {
-    "question":
+    question:
     "What is it called when Web Developers organize code in a readable and functional manner?",
-    "choices":["JavaScript","API","Refactoring","Interfacing",],
-		"answer": 2
+    choices:["JavaScript","API","Refactoring","Interfacing",],
+		answer: 2
     },
       //https://www.w3schools.com/java/java_data_types_non-prim.asp#:~:text=A%20primitive%20type%20has%20always,have%20all%20the%20same%20size.
     {
-      "question":
+      question:
 			"In JavaScript which of the following is not a primative values?",
-      "choices":["Number", "Function","Boolean","String"],
-			"answer": 1
+      choices:["Number", "Function","Boolean","String"],
+			answer: 1
 		},
     {
-      "question": "In CSS which of the following are used as selectors?",
-      "choices":["ID","Class","Global","They're all selectors"],
-			"answer": 3
+      question: "In CSS which of the following are used as selectors?",
+      choices:["ID","Class","Global","They're all selectors"],
+			answer: 3
 		},
     {
-      "question":"What is the planning and drawing out the design of a website before being built called?",
-      "choices":["Keyframing","Githubbing","Storyinboarding","Wireframing"],
-			"answer": 3
+      question:"What is the planning and drawing out the design of a website before being built called?",
+      choices:["Keyframing","Githubbing","Storyinboarding","Wireframing"],
+			answer: 3
     },
     {
        //paraphrased and forumlated from https://developer.mozilla.org/en-US/docs/Glossary/Element
-      "question": 'What is a typical element in Web Design?',
-      "choices":["A key value pair","Any part of a webpage that includes an opening tag with attrtirutes, encolsoed text, and a closing tag","A Boolean","A google"],
-			"answer": 1
+      question: 'What is a typical element in Web Design?',
+      choices:["A key value pair","Any part of a webpage that includes an opening tag with attrtirutes, encolsoed text, and a closing tag","A Boolean","A google"],
+			answer: 1
     },
     {
       //paraphrased from https://docs.github.com/en/repositories/creating-and-managing-repositories/about-repositories
-      "question":"A github repository is?",
-      "choices":[ "A folder on github.com that contains all of your project's files and revision history","Syntax","A mystery","A container for trash"],
-			"answer": 1
+      question:"A github repository is?",
+      choices:[ "A folder on github.com that contains all of your project's files and revision history","Syntax","A mystery","A container for trash"],
+			answer: 1
 		},
     {
       //https://www.hackreactor.com/blog/what-is-javascript-used-for paraphrased and referenced
-      "question":"JavaScript is?",
-      "choices":["A cilent-side and server-side programming language that allows a programmer make applications interactive","A web application","A node","A cup of coffee"],
-			"answer": 0
+      question:"JavaScript is?",
+      choices:["A cilent-side and server-side programming language that allows a programmer make applications interactive","A web application","A node","A cup of coffee"],
+			answer: 0
 		},
     {
       //https://www.git-tower.com/learn/git/faq/git-bash#:~:text=Git%20Bash%2C%20quite%20simply%2C%20is,built%20for%20Unix%2Dstyle%2
-      "question": "What is the difference between a method and a funciton in JavaScript?",
-			"choices":["They are the same thing", "One is server side and one is client side","A method is associated with an object while a function is an independent block of code",
+      question: "What is the difference between a method and a funciton in JavaScript?",
+			choices: ["They are the same thing", "One is server side and one is client side","A method is associated with an object while a function is an independent block of code",
 			"A method cannot be called while a function can"],
-  		"answer": 2
+  		answer: 2
 		},
     //https://www.educative.io/answers/what-is-the-difference-between-a-method-and-a-function
     {
-      "question":"What is a web application?",
-      "chocies": ["A program downloaded from the internet that runs on your local system","A browser based flash file","A program that runs on IOS",
+      question:"What is a web application?",
+      choices: ["A program downloaded from the internet that runs on your local system","A browser based flash file","A program that runs on IOS",
 			"A computer program that runs in a web browsers and perfroms taask over the internet"],
-			"answer": 3
+			answer: 3
     },
   ]
-console.log(questionsArr)
+
--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,4 @@
-
-
-//my global variable list to define variables I will manipulate in quiz
+///my global variable list to define variables I will manipulate in quiz
 const firstPage = document.getElementById("first-page");
 const mainHeader = document.getElementById("main-header");
 const highScore = document.getElementById("stats");
@@ -12,8 +10,8 @@ const questionTitles = document.getElementById("question-title");
 const choiceList = document.getElementById("choice-list");
 const choiceBtn = document.querySelectorAll(".choice-btn")
 const nextBtn = document.getElementById("next");
-const submitBtn = document.getElementById("log-score");
-
+const submitBtn = document.getElementById("submit");
+const finalScreen = document.getElementById("end-quiz")
 //array variables setting question index to move through our array. Score to add and subtract scores. and time for 10 seconds each question
 //we make a timerId to store our current time
 let currentQuestionIndex = 0;
@@ -22,13 +20,13 @@ let time = questionsArr.length * 10;
 let timerId;
 //make function to hide elements I want hidden on dom and start timer and show questions
 function startQuiz() {
-  firstPage.setAttribute("class", "hidden");
-  mainHeader.setAttribute("class", "hidden");
-  welcomeText.setAttribute("class", "hidden");
-  startBtn.setAttribute("class", "hidden");
-  quizContainer.removeAttribute("class");
-  questionTitles.removeAttribute("class");
-  choiceList.removeAttribute("class");
+	firstPage.setAttribute("class", "hidden");
+	mainHeader.setAttribute("class", "hidden");
+	welcomeText.setAttribute("class", "hidden");
+	startBtn.setAttribute("class", "hidden");
+	quizContainer.removeAttribute("class");
+	questionTitles.removeAttribute("class");
+	choiceList.removeAttribute("class");
 
 	nextBtn.removeAttribute("class")
 	startTimer();
@@ -36,17 +34,17 @@ function startQuiz() {
 
 //make function to start timer and count by one second each interval
 function startTimer() {
-  timerId = setInterval(function() {
-    time--;
-    timer.textContent = time;
-    if (time <= 0) {
-      clearInterval(timerId);
-    }
-  }, 1000);
+	timerId = setInterval(function () {
+		time--;
+		timer.textContent = time;
+		if (time <= 0) {
+			clearInterval(timerId);
+		}
+	}, 1000);
 }
 //function to create the elements that will go onto the HTML of the questions and answer choices using a a for loop to loop through the array's entire length generatng questions
 function showQuestions() {
-  const currentQuestion = questionsArr[currentQuestionIndex]
+	const currentQuestion = questionsArr[currentQuestionIndex]
 	questionTitles.textContent = currentQuestion.question
 	for (let i = 0; i < choiceBtn.length; i++) {
 		choiceBtn[i].textContent = currentQuestion.choices[i]
@@ -54,33 +52,42 @@ function showQuestions() {
 }
 //call to show our first question
 showQuestions();
-// loop through each question and choices in questionsArr and add event listener for all of the choice buttons
-choiceBtn.forEach(function(button) {
-  button.addEventListener("click", function(event) {
-    const selectedChoice = event.target.textContent
-    const currentQuestion = questionsArr[currentQuestionIndex]
-    //if user gets the answer correct add 5 points to their score
-		if (selectedChoice === currentQuestion.answer) {
-      score += 5;
-    //if a user gets a question incorrect deduct five points from their score
-		} else {
-      time -= 5;
-    }
-    currentQuestionIndex++;
-    if (currentQuestionIndex === questionsArr.length) {
-      // quiz is over, show the score submission form
-      // and stop the timer
-      clearInterval(timerId);
-      quizContainer.setAttribute("class", "hidden");
-      submitBtn.removeAttribute("class");
-      submitBtn.addEventListener("click", logScore);
-    } else {
-      // show the next question
-      showQuestions();
-    }
-  });
+//inputs each choice and adds 5 points for correct answer and takes away 5 seconds for incorrect answer
+function choiceInput(choice) {
+  const selectedChoice = choice.currentTarget.choice;
+  if (selectedChoice === questionsArr.answer) {
+    score += 5;
+  } else {
+    time -= 5;
+  }
+	//goes to next question
+  currentQuestionIndex++;
+	//tells quiz want to do at end of quiz
+  if (currentQuestionIndex === questionsArr.length) {
+    endQuiz();
+  } else {
+    showQuestions();
+  }
+}
+//ends the quiz and shows the end quiz to save the score
+function endQuiz() {
+  clearInterval(timerId);
+  quizContainer.setAttribute("class", "hidden");
+  questionTitles.setAttribute("class", 'hidden');
+  choiceList.setAttribute("class", "hidden");
+  nextBtn.setAttribute("class","hidden");
+  finalScreen.removeAttribute("class", "hidden");
+  submitBtn.addEventListener("click", logScore);
+}
+//adds an event listener to each choice to keep track of score
+choiceBtn.forEach(function (choice) {
+  choice.addEventListener("click", choiceInput);
 });
 
+//will be a function to log score in local storage
+function logScore(){
+
+}
 
 //start the quiz with quiz button
 startBtn.addEventListener("click", startQuiz);
